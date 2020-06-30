52500fe2e5c6c3b87a9765c58fd9d23a550bf81d
murthyr <rnmurthy@seas.upenn.edu>
2020-06-30 18:37:45 -0500

    Automated Draft Picks

--- app/src/main/java/com/example/a2020fantasyfootallsimulator/MainActivity2.java
+++ app/src/main/java/com/example/a2020fantasyfootallsimulator/MainActivity2.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -19,6 +28,9 @@ import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    public static ArrayList<Team> teamList = new ArrayList<Team>();
+    public static ArrayList<Player> playerList = new ArrayList<Player>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +40,7 @@ public class MainActivity2 extends AppCompatActivity {
         String selection = getSel.getExtras().getString("sel");
         final Integer num = Integer.valueOf(selection);
 
-        ArrayList<Team> teamList = new ArrayList<Team>();
+
 
         Random r = new Random();
         int playerNum = r.nextInt((num - 1) + 1) + 1;
@@ -55,7 +67,7 @@ public class MainActivity2 extends AppCompatActivity {
             }
         }
 
-        ArrayList<Player> playerList = new ArrayList<Player>();
+
 
         InputStream is = getResources().openRawResource(R.raw.ff_data);
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
@@ -85,6 +97,56 @@ public class MainActivity2 extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            int count = 1;
+            public void run() {
 
+                if (count < num * 14 + 1) {
+                    String[] arraySpinner = new String[playerList.size()];
+                    for(int i = 0; i < playerList.size(); i++) {
+                        arraySpinner[i] = playerList.get(i).getName() + "---" + playerList.get(i).getPosition() + "---" + Double.toString(playerList.get(i).getProjPoints());
+                    }
+                    final Spinner s = (Spinner) findViewById(R.id.availPlayers);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity2.this,
+                            android.R.layout.simple_spinner_item, arraySpinner);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s.setAdapter(adapter);
+
+                    TableLayout draft = (TableLayout)findViewById(R.id.tableLayout);
+                    TableRow tr =  new TableRow(MainActivity2.this);
+                    tr.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                    TextView c1 = new TextView(MainActivity2.this);
+                    c1.setText("Pick #" + Integer.toString(count) + "   ");
+                    c1.setTextSize(20);
+                    TextView c2 = new TextView(MainActivity2.this);
+                    String team = "";
+                    for(int k = 0; k < teamList.size(); k++) {
+                        if(teamList.get(k).getPicks().contains(count))
+                            team = teamList.get(k).getName();
+                    }
+                    c2.setText(team + "   ");
+                    c2.setTextSize(20);
+
+                    TextView c3 = new TextView(MainActivity2.this);
+                    Random rn = new Random();
+                    int rand = rn.nextInt(2 - 0 + 1) + 0;
+                    Player pl = playerList.remove(rand);
+                    c3.setText(pl.getName());
+                    c3.setTextSize(20);
+
+                    tr.addView(c1);
+                    tr.addView(c2);
+                    tr.addView(c3);
+                    draft.addView(tr);
+                    count++;
+
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+        handler.post(runnable);
     }
+
 }
+
